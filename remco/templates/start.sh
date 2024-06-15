e89799408fe479da5d93d0f7a5637cac546a04c6
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2024-06-15 12:21:18 -0700

    Update start.sh

--- remco/templates/start.sh
+++ remco/templates/start.sh
@@ -1,11 +1,11 @@
 ./StartServer.sh \
-  -SteamServerName="{{ getv("/soulmask/servername", "Soulmask Dedicated Server") }}" \
+  -SteamServerName="{{ getv("/soulmask/servername", "SoulmaskDedicatedServer") }}" \
   -Port="{{ getv("/soulmask/port", "8777") }}" \
   -QueryPort="{{ getv("/soulmask/queryport", "27015") }}" \
-  -EchoPort="{{ getv("/soulmask/echoyport", "18888") }}" \
-{% if getv("/soulmask/pve", "false")|lower == "true" %}
-  -pve \
-{% endif %}
+  -EchoPort="{{ getv("/soulmask/echoport", "18888") }}" \
   -MaxPlayers="{{ getv("/soulmask/maxplayers", "50") }}" \
   -PSW="{{ getv("/soulmask/password", "") }}" \
   -adminpsw="{{ getv("/soulmask/admin/password", "Secret123") }}"
+  {% if getv("/soulmask/pve", "false")|lower == "true" %}
+    -pve \
+  {% endif %}
