a42b2871da8ad05fd59fa1d3b7a6f90eaf0e9832
PapaBearDoes <73200436+PapaBearDoes@users.noreply.github.com>
2024-06-16 07:43:58 -0700

    Update start.sh

--- remco/templates/start.sh
+++ remco/templates/start.sh
@@ -1,11 +1,21 @@
-./StartServer.sh \
-  -SteamServerName="{{ getv("/soulmask/servername", "SoulmaskDedicatedServer") }}" \
-  -Port="{{ getv("/soulmask/port", "8777") }}" \
-  -QueryPort="{{ getv("/soulmask/queryport", "27015") }}" \
-  -EchoPort="{{ getv("/soulmask/echoport", "18888") }}" \
-  -MaxPlayers="{{ getv("/soulmask/maxplayers", "50") }}" \
-  -PSW="{{ getv("/soulmask/password", "") }}" \
-  -adminpsw="{{ getv("/soulmask/admin/password", "Secret123") }}"
-  {% if getv("/soulmask/pve", "false")|lower == "true" %}
-    -pve \
-  {% endif %}
+SERVERNAME='{{ getv("/soulmask/servername", "SoulmaskDedicatedServer") }}'
+PORT='{{ getv("/soulmask/port", "8777") }}'
+QUERYPORT='{{ getv("/soulmask/queryport", "27015") }}'
+ECHOPORT='{{ getv("/soulmask/echoport", "18888") }}'
+MAXPLAYERS='{{ getv("/soulmask/maxplayers", "50") }}'
+PSW='{{ getv("/soulmask/password", "") }}'
+ADMINPSW='{{ getv("/soulmask/admin/password", "Secret123") }}'
+PVE='{% if getv("/soulmask/pve", "false")|lower == "true" %}-pve{% endif %}'
+echo ""
+echo "#####################################"
+echo "Servername = "${SERVERNAME}
+echo "GamePort = "${PORT}
+echo "QueryPort = "${QUERYPORT}
+echo "EchoPort = "${ECHOPORT}
+echo "MaxPlayers ="${MAXPLAYERS}
+echo "Server Password = ********"
+echo "Admin Password = ********"
+echo ${PVE}
+echo "#####################################"
+echo ""
+./StartServer.sh -SteamServerName="${SERVERNAME}" -Port="${PORT}" -QueryPort="${QUERYPORT}" -EchoPort="${ECHOPORT}" -MaxPlayers="${MAXPLAYERS}" -PSW="${PSW}" -adminpsw="${ADMINPSW}" ${PVE}
